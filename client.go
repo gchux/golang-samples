d49310c222ae765c99b5f79f621594d8c8cf1b66
javalladares <javalladares@google.com>
2024-10-15 00:20:20 +0000

    [fix]: client should accept tokens

--- client.go
+++ client.go
@@ -23,13 +23,19 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	grpcMetadata "google.golang.org/grpc/metadata"
 )
 
 const (
@@ -37,23 +43,53 @@ const (
 )
 
 var (
-	addr = flag.String("addr", "localhost:8080", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
+	addr   = flag.String("addr", "localhost:8080", "the address to connect to")
+	name   = flag.String("name", defaultName, "Name to greet")
+	secure = flag.Bool("secure", false, "use TLS")
+	token  = flag.String("token", "", "Identity Token")
 )
 
 func main() {
 	flag.Parse()
+
+	var opts []grpc.DialOption
+
+	if *addr != "" {
+		opts = append(opts, grpc.WithAuthority(*addr))
+	}
+
+	fmt.Println(*addr)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if *secure {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	} else {
+		// Note: On the Windows platform, use of x509.SystemCertPool() requires
+		// go version 1.18 or higher.
+		systemRoots, err := x509.SystemCertPool()
+		if err != nil {
+			os.Exit(1)
+		}
+		cred := credentials.NewTLS(&tls.Config{
+			RootCAs: systemRoots,
+		})
+		opts = append(opts, grpc.WithTransportCredentials(cred))
+	}
+
 	// Set up a connection to the server.
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(*addr, opts...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
 
+	ctx = grpcMetadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+*token)
+
 	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)

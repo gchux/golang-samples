ae7f866578ea40f25ed0165129315267b60d8401
javalladares <javalladares@google.com>
2024-10-17 21:15:27 +0000

    [FIX]: test improvments

--- test-grpc-web-proxy.bash
+++ test-grpc-web-proxy.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-curlie --raw --http1.1 -ivL -XPOST \
-  -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
-  -H 'User-Agent: grpc-web-javascript/0.1' \
+ID_TOKEN="$(gcloud auth print-identity-token | tr -d '\n')"
+ENDPOINT='https://helloworld-grpc-web-proxy-114063878166.us-west4.run.app/helloworld.Greeter/SayHello'
+
+curl --raw -D - -iv \
+  --http1.1 -XOPTIONS -H "Authorization: Bearer ${ID_TOKEN}" \
+  -H 'Access-Control-Request-Method: POST' \
+  -H 'Access-Control-Request-Headers: authorization,x-serverless-authorization,x-grpc-web,content-type,accept,user-agent,x-request-id,x-client-id'  \
+  -H 'Origin: https//localhost' "${ENDPOINT}" \
+  --next \
+  --http1.1 -XPOST -H "Authorization: Bearer ${ID_TOKEN}" \
   -H 'Accept: application/grpc-web-text' \
   -H 'Content-Type: application/grpc-web-text' \
-  -H 'X-Grpc-Web: 1' \
-  'https://helloworld-grpc-web-proxy-114063878166.us-west4.run.app/helloworld.Greeter/SayHello' \
-  --data 'AAAAAAYKBHRlc3Q=' -D -
+  -H 'X-Grpc-Web: 1' "${ENDPOINT}" \
+  --data 'AAAAAAYKBHRlc3Q='

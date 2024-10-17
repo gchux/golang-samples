e4c4b4c1cc344d3905faa303aebf5ec32818a3f8
javalladares <javalladares@google.com>
2024-10-17 03:54:48 +0000

    [NEW]: gRPC web proxy test script

---
+++ test-grpc-web-proxy.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curlie --raw --http1.1 -ivL -XPOST \
+  -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
+  -H 'User-Agent: grpc-web-javascript/0.1' \
+  -H 'Accept: application/grpc-web-text' \
+  -H 'Content-Type: application/grpc-web-text' \
+  -H 'X-Grpc-Web: 1' \
+  'https://helloworld-grpc-web-proxy-114063878166.us-west4.run.app/helloworld.Greeter/SayHello' \
+  --data 'AAAAADoSFkFtaW4gTmFzaXJpIFhlbm9uIEdSUEMYNjoePHNjcmlwdD5hbGVydChvcmlnaW4pPC9zY3JpcHQ+' \
+  -D -
